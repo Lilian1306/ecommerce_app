0003223cdc857fd6c8357185e0e0ac7f9152fe2c
DaniM0613 <wwh.rj.92@gmail.com>
2024-11-01 18:36:48 -0600

    Creando el componente de NewsLetterBox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Order from './pages/Orders'
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 export default function App() {
@@ -27,6 +28,7 @@ export default function App() {
         <Route path="/place-order" element={<PlaceOrder/>}/>
         <Route path="/order" element={<Order/>}/>
        </Routes>
+       <Footer/>
     </div>
   )
 }
