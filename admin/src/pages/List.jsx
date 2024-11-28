c9b4bb414040598e38d0fd6e41551807db51ba38
DaniM0613 <wwh.rj.92@gmail.com>
2024-11-28 15:06:58 -0600

    Last part

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -44,16 +44,16 @@ export default function List({token}) {
 
   return (
     <>
-     <p className='mb-2'>All Products List</p>
+     <p className='mb-2'>Listado de productos</p>
       <div className='flex flex-col gap-2'>
 
         {/** List Table Title */}
          <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
-           <b>Image</b>
-           <b>Name</b>
-           <b>Category</b>
-           <b>Price</b>
-           <b className='text-center'>Action</b>
+           <b>Imagen</b>
+           <b>Nombre</b>
+           <b>Categoria</b>
+           <b>Precio</b>
+           <b className='text-center'>Eliminar</b>
          </div>
 
          {/** ----- Product List ------- */}
